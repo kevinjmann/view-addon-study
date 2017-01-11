89d5183deda3d2991c7e3efacb6e1aeb1306f373
eddi89 <eduard.schaf@web.de>
2017-01-11 19:11:48 +0100

    Add tests for view.js
    All tests are now available.

--- src/viewWE/content_scripts/js/view.js
+++ src/viewWE/content_scripts/js/view.js
@@ -1,4 +1,4 @@
-var view = {
+const view = {
   // General options
   serverURL: "https://view.aleks.bg",
   servletURL: "https://view.aleks.bg/view",
@@ -28,76 +28,95 @@ var view = {
   // e.g. topic = "RusNouns", topic name = "rusnouns"
   topicName: "unselected",
 
-  /*
-   * Save general options to the storage.
-   * This options can't be changed by the user.
+  /**
+   * Save general options to the storage
+   * and retrieve changed options.
    */
   saveGeneralOptions: function() {
-    console.log("saveGeneralOptions()");
     chrome.runtime.sendMessage({
         msg: "call sendTopics"
       }, function(response) {
-        console.log("received response: got topics JSON object.");
         chrome.storage.local.get([
           "userEmail",
           "userid",
           "enabled"
-        ], function(res) {
+        ], function(storageItems) {
           view.topics = response.topics;
-          chrome.storage.local.set({
-            serverURL: view.serverURL,
-            servletURL: view.servletURL,
-            cookie_name: view.cookie_name,
-            cookie_path: view.cookie_path,
-            ajaxTimeout: view.ajaxTimeout
-          });
-          // set the user email and user id
-          if (res.userid == undefined) { //  to the default
-            chrome.storage.local.set({
-              userEmail: view.userEmail,
-              userid: view.userid
-            });
-          }
-          else { // to the stored values
-            view.userEmail = res.userEmail;
-            view.userid = res.userid;
-          }
-
-          // set the enabled auto-run true/false option
-          if (res.enabled == undefined) { // to the default
-            chrome.storage.local.set({
-              enabled: view.enabled
-            });
-          }
+          view.setAllGeneralOptions(storageItems);
         });
       }
     );
   },
 
   /**
-   * Save all user options from the options page.
+   * Set all options, including eventually changed
+   * ones from storage.
    *
-   * @param {object} storageItems the storage items
+   * @param  {object} storageItems changeable items from storage
    */
-  saveUserOptions: function(storageItems) {
-    console.log("saveUserOptions()");
-    if (storageItems.fixedOrPercentage !== undefined) {
-      view.fixedOrPercentage = storageItems.fixedOrPercentage;
-      view.fixedNumberOfExercises = storageItems.fixedNumberOfExercises;
-      view.percentageOfExercises = storageItems.percentageOfExercises;
-      view.choiceMode = storageItems.choiceMode;
-      view.firstOffset = storageItems.firstOffset;
-      view.intervalSize = storageItems.intervalSize;
-      view.showInst = storageItems.showInst;
+  setAllGeneralOptions: function(storageItems) {
+    view.setFixedGeneralOptions();
+    view.setMutableGeneralOptions(storageItems);
+  },
+
+  /**
+   * Set all general options that aren't changeable.
+   */
+  setFixedGeneralOptions: function() {
+    chrome.storage.local.set({
+      serverURL: view.serverURL,
+      servletURL: view.servletURL,
+      cookie_name: view.cookie_name,
+      cookie_path: view.cookie_path,
+      ajaxTimeout: view.ajaxTimeout
+    });
+  },
+
+  /**
+   * Set all general options that can be changed during runtime.
+   *
+   * @param  {object} storageItems changeable items from storage
+   */
+  setMutableGeneralOptions: function(storageItems) {
+    view.setEmailAndId(storageItems.userEmail, storageItems.userid);
+    view.setAutoEnhance(storageItems.enabled);
+  },
+
+  /**
+   * Set user email and id.
+   *
+   * @param email the email address from the user
+   * @param id the user id
+   */
+  setEmailAndId: function(email, id) {
+    if (id == undefined) {
+      chrome.storage.local.set({
+        userEmail: view.userEmail,
+        userid: view.userid
+      });
+    }
+    else {
+      view.userEmail = email;
+      view.userid = id;
     }
   },
 
-  /*
+  /**
+   * Set whether a page should be enhanced on the spot or not.
+   *
+   * @param enabled true if the page should be auto enhanced, false otherwise
+   */
+  setAutoEnhance: function(enabled) {
+    if (enabled == undefined) {
+      chrome.storage.local.set({enabled: view.enabled});
+    }
+  },
+
+  /**
    * Initialize all user options and make them accessible to VIEW.
    * Afterwards start enhancing.
    */
   startToEnhance: function() {
-    console.log("startToEnhance()");
     chrome.storage.local.get([
       "fixedOrPercentage",
       "fixedNumberOfExercises",
@@ -110,21 +129,43 @@ var view = {
       "language",
       "topic",
       "activity"
-    ], function(storageItems) {// TODO: this should be handled in the toolbar instead
-      // save user options, as they might have changed
+    ], function(storageItems) {
       view.saveUserOptions(storageItems);
 
-      // enabled, language, topic and activity selections (default)
-      view.enabled = storageItems.enabled;
-      view.language = storageItems.language;
-      view.topic = storageItems.topic;
-      view.activity = storageItems.activity;
+      view.saveSelections(storageItems);
 
-      // set the topic name
       view.topicName = view.interaction.getTopicName(view.topic);
 
-      // start enhancing the page
       view.interaction.enhance();
     });
+  },
+
+  /**
+   * Save all user options from the options page.
+   *
+   * @param {object} storageItems the storage items
+   */
+  saveUserOptions: function(storageItems) {
+    if (storageItems.fixedOrPercentage !== undefined) {
+      view.fixedOrPercentage = storageItems.fixedOrPercentage;
+      view.fixedNumberOfExercises = storageItems.fixedNumberOfExercises;
+      view.percentageOfExercises = storageItems.percentageOfExercises;
+      view.choiceMode = storageItems.choiceMode;
+      view.firstOffset = storageItems.firstOffset;
+      view.intervalSize = storageItems.intervalSize;
+      view.showInst = storageItems.showInst;
+    }
+  },
+
+  /**
+   * Set all user selections.
+   *
+   * @param {object} storageItems the storage items
+   */
+  saveSelections: function(storageItems) {
+    view.enabled = storageItems.enabled;
+    view.language = storageItems.language;
+    view.topic = storageItems.topic;
+    view.activity = storageItems.activity;
   }
 };
