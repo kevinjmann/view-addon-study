26bdae68b516ffda3da2f632ddd35bb1b355ed2f
eddi89 <eduard.schaf@web.de>
2017-03-30 21:31:50 +0200

    Put initToolbar and toggleToolbar into a separate file

---
+++ src/viewWE/content_scripts/js/toolbar-utils.js
@@ -0,0 +1,80 @@
+view.toolbarUtils = {
+  toolbarUI: undefined,
+
+  /**
+   * Create the toolbar ui iframe and inject it in the current page.
+   *
+   * @returns the iframe containing the toolbar
+   */
+  initToolbar: function() {
+    const toolbarHTML = chrome.runtime.getURL("toolbar/toolbar.html");
+
+    const $iframe = $("<iframe>");
+    $iframe.attr("id", "view-toolbar-iframe");
+    $iframe.attr("src", toolbarHTML);
+
+    const $Body = $("body");
+
+    view.toolbarUtils.addContainer($Body);
+
+    view.VIEWmenu.add();
+
+    $Body.prepend($iframe);
+
+    return view.toolbarUtils.toolbarUI = $iframe;
+  },
+
+  /**
+   * Add a container that wraps all children inside the parent element
+   * given the selector of the parent.
+   *
+   * @param {Object} $Element the parent element
+   */
+  addContainer: function($Element) {
+    const $Container = $("<div id='wertiview-container'>");
+
+    let $Content = $("<div id='wertiview-content'>");
+
+    $Element.children().wrapAll($Content);
+
+    $Content = $("#wertiview-content");
+
+    $Container.append($Content);
+
+    $Container.addClass("down");
+
+    $Element.append($Container);
+  },
+
+  /**
+   * Toggle the toolbar directly if it already exists,
+   * initialize it otherwise.
+   * Hide the view menu in case it is still open.
+   */
+  toggleToolbar: function() {
+    const toolbarUI = view.toolbarUtils.toolbarUI;
+    if (toolbarUI) {
+      toolbarUI.toggle();
+      view.toolbarUtils.moveContainer();
+    } else {
+      view.setGeneralOptions();
+      view.toolbarUtils.initToolbar();
+    }
+  },
+
+  /**
+   * Move the container down when the toolbar is visible,
+   * but move it back and hide the VIEW menu otherwise.
+   */
+  moveContainer: function() {
+    const $Container = $("#wertiview-container");
+
+    if (view.toolbarUtils.toolbarUI.is(":visible")) {
+      $Container.addClass("down");
+    }
+    else {
+      view.VIEWmenu.hide();
+      $Container.removeClass("down");
+    }
+  }
+};
