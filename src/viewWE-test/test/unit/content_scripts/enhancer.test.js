205e5b41c9de366297c4b82d57809b9bcf44b783
eddi89 <eduard.schaf@web.de>
2017-04-06 22:11:29 +0200

    Add tests for function enhance()

--- src/viewWE-test/test/unit/content_scripts/enhancer.test.js
+++ src/viewWE-test/test/unit/content_scripts/enhancer.test.js
@@ -16,6 +16,11 @@ describe("enhancer.js", function() {
   afterEach(function() {
     sandbox.restore();
     fixture.cleanup();
+    view.activity = "";
+    view.showInst = false;
+    view.topics = {};
+    view.topic = "";
+    view.language = "";
   });
 
   describe("jquery selectors", function() {
@@ -37,6 +42,65 @@ describe("enhancer.js", function() {
 
       expect($("#wertiview-content").length).to.be.above(0);
       expect($("#ru-no-markup-body").length).to.be.above(0);
+    });
+  });
+
+  describe("init", function() {
+    it("should call restoreToOriginal(), as there is still server markup in the page", function() {
+      const restoreToOriginalSpy = sandbox.spy(view.enhancer, "restoreToOriginal");
+
+      $("body").append("<viewenhancement>");
+
+      view.enhancer.enhance();
+
+      sinon.assert.calledOnce(restoreToOriginalSpy);
+
+      $("viewenhancement").remove();
+    });
+
+    it("should call blur.add(), as the activity is 'cloze'", function() {
+      const addSpy = sandbox.spy(view.blur, "add");
+
+      view.activity = "cloze";
+
+      view.enhancer.enhance();
+
+      sinon.assert.calledOnce(addSpy);
+    });
+
+    it("should call constructInstruction(), as showInst is enabled", function() {
+      const constructInstructionSpy = sandbox.spy(view.enhancer, "constructInstruction");
+      const jsonData = fixture.load("fixtures/json/nouns.json", true);
+
+      view.showInst = true;
+      view.topic = "nouns";
+      view.language = "ru";
+      view.activity = "color";
+      view.topics = {nouns: jsonData};
+
+      view.enhancer.enhance();
+
+      sinon.assert.calledOnce(constructInstructionSpy);
+    });
+
+    it("should call requestToToggleElement(msg, selector)", function() {
+      const requestToToggleElementSpy = sandbox.spy(view.enhancer, "requestToToggleElement");
+
+      view.enhancer.enhance();
+
+      sinon.assert.calledOnce(requestToToggleElementSpy);
+      sinon.assert.calledWithExactly(requestToToggleElementSpy,
+        "show element",
+        "#wertiview-toolbar-abort-button"
+      );
+    });
+
+    it("should call createActivityData()", function() {
+      const createActivityDataSpy = sandbox.spy(view.enhancer, "createActivityData");
+
+      view.enhancer.enhance();
+
+      sinon.assert.calledOnce(createActivityDataSpy);
     });
   });
 });
