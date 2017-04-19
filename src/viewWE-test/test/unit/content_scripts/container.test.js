9c2fe2a21b5a10d5df7d8a81b9513234b079ad3a
eddi89 <eduard.schaf@web.de>
2017-04-19 20:30:10 +0200

    Test if originalContent gets filled as expected

--- src/viewWE-test/test/unit/content_scripts/container.test.js
+++ src/viewWE-test/test/unit/content_scripts/container.test.js
@@ -21,6 +21,7 @@ describe("container.js", function() {
     $("#view-toolbar-iframe").remove();
     $("body").append($("#ru-no-markup-body"));
     $("#wertiview-container").remove();
+    view.originalContent = "";
     fixture.cleanup();
   });
 
@@ -44,6 +45,26 @@ describe("container.js", function() {
   });
 
   describe("add", function() {
+    it("should store the body content into view.originalContent", function() {
+      fixture.load("/fixtures/ru-no-markup.html");
+
+      const fixtureInnerHTML = $("#ru-no-markup-body").html();
+      const $FixtureBody = $("<div>");
+
+      $FixtureBody.html(fixtureInnerHTML);
+
+      $("body").append($FixtureBody);
+
+      view.container.add($FixtureBody);
+
+      const $ContentChildren = $("#wertiview-content").children();
+
+      expect($ContentChildren.get(0)).to.equal(view.originalContent.get(0));
+      expect($ContentChildren.get(1)).to.equal(view.originalContent.get(1));
+
+      $FixtureBody.remove();
+    });
+
     it("should wrap all body content of 'ru-no-markup.html' into a new element", function() {
       fixture.load("/fixtures/ru-no-markup.html");
 
