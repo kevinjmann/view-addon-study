cd8bb15801fb771d5e20045725d2a166f4c8cf52
eddi89 <eduard.schaf@web.de>
2017-06-08 00:49:15 +0200

    Add human labels for Feedback messages.

---
+++ src/viewWE/content_scripts/js/assessment.js
@@ -0,0 +1,15 @@
+view.assessment = {
+  PROVIDED_SOLUTION: "Solution was provided",
+  EXACT_MATCH: "Perfect solution",
+  LOWERCASE_MATCH: "Capitalization error",
+  SPACE_MATCH: "Extra spaces",
+  NO_DIAGNOSIS: "Unknown error",
+  WRONG_LEMMA: "Word choice error in lemma",
+  WRONG_POS: "Word choice error in part-of-speech",
+  WRONG_NUMBER: "Word form error in number",
+  WRONG_CASE: "Word form error in case",
+  WRONG_NUMBER_AND_CASE: "Word form error in number and case",
+  WRONG_SPELLING: "Spelling error",
+  WRONG_SPELLING_UNSTRESSED_O: "Spelling error in unstressed 'o' position",
+  WRONG_SPELLING_UNSTRESSED_E: "Spelling error in unstressed 'e' position"
+};
