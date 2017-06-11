aaebb018e8a315890f100535581d99d63700d1e6
Aleksandar Dimitrov <a.dimitrov@seidemann-web.com>
2017-06-11 21:39:05 +0200

    Also move html files

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -4,19 +4,21 @@ const glob = require("glob");
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const CssTextPlugin = new ExtractTextPlugin("[name].css");
+const HtmlTextPlugin = new ExtractTextPlugin("[name].html");
+
 const addon = path.resolve(__dirname, "addon");
 const source_path = path.resolve(__dirname, "./viewWE");
 
 module.exports = {
   entry: {
     'background/background': path.resolve(source_path, "background.js"),
-    'toolbar/toolbar': [
-      path.resolve(source_path, "toolbar/toolbar.js"),
-      path.resolve(source_path, "toolbar/toolbar.css")
-    ],
+    'toolbar/toolbar': glob.sync(path.resolve(source_path, "toolbar/toolbar.*")),
+    'options/options': glob.sync(path.resolve(source_path, "options/options.*")),
     'content_scripts/view': [].concat(
       glob.sync(path.resolve(source_path, "content_scripts/js/**/*.js")),
-      glob.sync(path.resolve(source_path, "content_scripts/css/**/*.css"))
+      glob.sync(path.resolve(source_path, "content_scripts/css/**/*.css")),
+      glob.sync(path.resolve(source_path, "content_scripts/css/**/*.html"))
     )
   },
   devtool: 'source-map',
@@ -31,17 +33,22 @@ module.exports = {
         "to": path.resolve(addon, "manifest.json")
       }
     ]),
-    new ExtractTextPlugin("[name].css")
+    CssTextPlugin,
+    HtmlTextPlugin
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
+        loader: CssTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader!sass-loader"})
+        loader: CssTextPlugin.extract({fallback: "style-loader", use: "css-loader!sass-loader"})
+      },
+      {
+        test: /\.html$/,
+        loader: HtmlTextPlugin.extract({use: "raw-loader"})
       }
     ]
   }
