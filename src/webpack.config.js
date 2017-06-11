a02284d55029a3022442ded2ae2be3a739e6256e
Aleksandar Dimitrov <a.dimitrov@seidemann-web.com>
2017-06-11 19:48:11 +0200

    Add content_scripts

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,12 +1,15 @@
 const path = require("path");
+const glob = require("glob");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const addon = path.resolve(__dirname, "addon");
+const content_scripts_path = path.resolve(__dirname, "./viewWE/content_scripts/");
 
 module.exports = {
   entry: {
-    'background/index': "./viewWE/background.js",
-    'content_scripts/view': "./viewWE/content_scripts/js/view.js"
+    'background/background': "./viewWE/background.js",
+    'toolbar/toolbar': "./viewWE/toolbar/toolbar.js",
+    'content_scripts/view': glob.sync(path.resolve(content_scripts_path, "js/**/*.js"))
   },
   devtool: 'source-map',
   output: {
@@ -20,5 +23,25 @@ module.exports = {
         "to": path.resolve(addon, "manifest.json")
       }
     ])
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: { sourceMap : true }
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap : true }
+          }
+        ]
+      }
+    ]
+  }
 };
