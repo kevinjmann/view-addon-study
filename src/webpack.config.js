6326b479c734f359bfca51bc6413fcd695d22ff8
Aleksandar Dimitrov <a.dimitrov@seidemann-web.com>
2017-06-11 20:48:35 +0200

    Factour out paths

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -5,15 +5,15 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const addon = path.resolve(__dirname, "addon");
-const content_scripts_path = path.resolve(__dirname, "./viewWE/content_scripts/");
+const source_path = path.resolve(__dirname, "./viewWE");
 
 module.exports = {
   entry: {
     'background/background': "./viewWE/background.js",
     'toolbar/toolbar': "./viewWE/toolbar/toolbar.js",
     'content_scripts/view': [].concat(
-      glob.sync(path.resolve(content_scripts_path, "js/**/*.js")),
-      glob.sync(path.resolve(content_scripts_path, "css/**/*.css"))
+      glob.sync(path.resolve(source_path, "content_scripts/js/**/*.js")),
+      glob.sync(path.resolve(source_path, "content_scripts/css/**/*.css"))
     )
   },
   devtool: 'source-map',
