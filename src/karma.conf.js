316dc74798019bda9cb5b60cd91453cde057b613
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-06-23 22:36:29 +0200

    Add fixture preprocessors

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -3,16 +3,26 @@ const webpackConfig = require('./webpack.config.js');
 module.exports = function(config) {
   config.set({
     files: [
-      'viewWE-test/**/*.test.js'
+      'viewWE-test/**/*.test.js',
+      'spec/fixtures/**/*'
     ],
 
     preprocessors: {
-      'viewWE-test/**/*.test.js': ['webpack', 'sourcemap']
+      // test files
+      'viewWE-test/**/*.test.js': ['webpack', 'sourcemap'],
+
+      // fixtures
+      '**/*.html'   : ['html2js'],
+      '**/*.json'   : ['json_fixtures']
+    },
+
+    jsonFixturesPreprocessor: {
+      variableName: '__json__'
     },
 
     webpack: webpackConfig,
 
-    frameworks: ['chai', 'mocha', 'sinon'],
+    frameworks: ['chai', 'mocha', 'sinon', 'fixture'],
 
     browsers: ['Firefox', 'ChromiumHeadless'],
 
