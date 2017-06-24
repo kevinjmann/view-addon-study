b871a8dd4493e1975e9cd2f31010692c4bf2747e
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-06-24 10:55:52 +0200

    Factor out selector cache

---
+++ src/viewWE/SelectorCache.js
@@ -0,0 +1,28 @@
+const $ = require('jquery');
+
+/**
+ * From: https://gist.github.com/jtsternberg/1e03f5fd5be8427170c5
+ * Caches jquery selectors when they are requested, so that each element
+ * is only selected once. An element can be reset, if necessary.
+ * For selectors like $(this) in e.g. selection menus you should always
+ * reset the element or don't use it at all.
+ * Be careful of selections that won't stay the same.
+ *
+ * @returns {{get: get_from_cache}} use: let cache = new Selector_Cache();
+ * cache.get(selector) or
+ * cache.get(selector, true) if the selected element should be reset
+ * @constructor initialized with: new Selector_Cache()
+ */
+module.exports = function () {
+  const collection = {};
+
+  function get_from_cache(selector, reset) {
+    if (undefined === collection[selector] || true === reset) {
+      collection[selector] = $(selector);
+    }
+
+    return collection[selector];
+  }
+
+  return {get: get_from_cache};
+};
--- src/viewWE/options/options.js
+++ src/viewWE/options/options.js
@@ -1,31 +1,5 @@
 const $ = require('jquery');
-
-/**
- * From: https://gist.github.com/jtsternberg/1e03f5fd5be8427170c5
- * Caches jquery selectors when they are requested, so that each element
- * is only selected once. An element can be reset, if necessary.
- * For selectors like $(this) in e.g. selection menus you should always
- * reset the element or don't use it at all.
- * Be careful of selections that won't stay the same.
- *
- * @returns {{get: get_from_cache}} use: let cache = new Selector_Cache();
- * cache.get(selector) or
- * cache.get(selector, true) if the selected element should be reset
- * @constructor initialized with: new Selector_Cache()
- */
-function Selector_Cache() {
-  const collection = {};
-
-  function get_from_cache(selector, reset) {
-    if (undefined === collection[selector] || true === reset) {
-      collection[selector] = $(selector);
-    }
-
-    return collection[selector];
-  }
-
-  return {get: get_from_cache};
-}
+const Selector_Cache = require('./../SelectorCache.js');
 
 const viewOptions = {
   $cache: new Selector_Cache(),
@@ -306,6 +280,8 @@ const viewOptions = {
     viewOptions.$cache.get("#" + selectionId).prop("checked", true);
   }
 };
+
+module.exports = viewOptions;
 
 /**
  * Initialize the options when the document is ready.
