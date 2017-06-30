f0b38001c01feaf3bee56bdf205a6cd94b455d3a
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-06-30 22:18:02 +0200

    Add comments to webpack.conf.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
+/**
+ * Global compilation configuration file for webpack
+ *
+ * Output is in /addon
+ *
+ * It produces three bundles:
+ *
+ * - /background/background.js
+ * - /content_scripts/view.js
+ * - /options/options.js
+ *
+ * It also copies the necessary HTML files and icons.
+ */
+
 const path = require("path");
 const glob = require("glob");
 
@@ -12,8 +26,14 @@ const source_path = path.resolve(__dirname, "./viewWE");
 
 module.exports = {
   entry: {
+    /** background script */
     'background/background': path.resolve(source_path, "background.js"),
+
+    /** options script and its assets, used for addon options.
+        The HTML is extracted with the extract text plugin */
     'options/options': glob.sync(path.resolve(source_path, "options/options.*")),
+
+    /** content scripts, emitted as a single bundle, including CSS */
     'content_scripts/view': [
       path.resolve(source_path, "content_scripts/js/messageHandler.js")
     ].concat(
@@ -27,6 +47,7 @@ module.exports = {
   },
   plugins: [
     new CopyWebpackPlugin([
+      /** Copies the icons and the manifest to the target location */
       {
         from: path.resolve(source_path, "manifest.json"),
         to:   path.resolve(addon, "manifest.json")
@@ -40,14 +61,18 @@ module.exports = {
     HtmlTextPlugin
   ],
   resolve: {
+    /** This hack is here because sinon uses 'require' in buggy ways */
     alias: {
       sinon: path.resolve(__dirname, 'node_modules/sinon/pkg/sinon.js') // require dist version instead
     }
   },
   module: {
+    /** See above comment on resolve.alias.sinon */
     noParse: [ /node_modules\/sinon$/ ],
+
     rules: [
       {
+        /** run all js files through babel loader with es2015, except node_modules */
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
@@ -59,26 +84,33 @@ module.exports = {
         }
       },
       {
+        /** CSS files are loaded with the css-loader and extracted */
         test: /\.css$/,
         loader: CssTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
       },
       {
+        /** SCSS files are first run through the SCSS compiler, and then processd just like CSS files */
         test: /\.scss$/,
         loader: CssTextPlugin.extract({fallback: "style-loader", use: "css-loader!sass-loader"})
       },
       {
+        /** HTML files in content_scripts are included in the JS bundle, and not extracted. */
         test: /.*content_scripts.*\.html$/,
         loader: "html-loader"
       },
       {
+        /** PNGs are loaded inline in JS bundles */
         test: /.*\.png$/,
         loader: "url-loader?mimetype=image/png"
       },
       {
+        /** GIFs are loaded inline in JS bundles */
         test: /.*\.gif$/,
         loader: "url-loader?mimetype=image/gif"
       },
       {
+        /** The html file for options is extracted separately, as it needs
+            to be present for the webextensions options mechanism to work.*/
         test: /.*(options).*\.html$/,
         loader: HtmlTextPlugin.extract({use: "raw-loader"})
       }
