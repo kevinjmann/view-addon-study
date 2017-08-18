3b002e26d25df73887fdbdaef06243668b524888
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-08-18 20:25:44 +0200

    Add some error case tests to FirebaseAdapter

--- viewWE-test/test/unit/firebaseAdapter.test.js
+++ viewWE-test/test/unit/firebaseAdapter.test.js
@@ -1,17 +1,34 @@
+import firebase from 'firebase';
 import FirebaseAdapter from '../../../viewWE/firebaseAdapter.js';
 import view from '../../../viewWE/content_scripts/js/view.js';
 
 describe("FirebaseAdapter", () => {
   let sandbox;
-  beforeEach(() => {
+  let addNotification;
+  before(() => {
     sandbox = sinon.sandbox.create();
   });
 
-  it("Displays a notification when firebase throws an error", () => {
-    const firebase = new FirebaseAdapter();
-    const addNotification = sandbox.stub(view.notification, "add");
-    firebase.initialize({foo: "bar"});
+  afterEach(() => {
+    sandbox.restore();
+  });
 
+  it("Displays a notification when firebase throws an error", () => {
+    addNotification = sandbox.stub(view.notification, "add");
+    new FirebaseAdapter({foo: "bar"});
     sinon.assert.calledOnce(addNotification);
+  });
+
+  it("Doesn't log a notification on duplicate app error", () => {
+    addNotification = sandbox.stub(view.notification, "add");
+    const initializeFirebase = sandbox.stub(firebase, "initializeApp");
+    initializeFirebase.throws({
+      "name": "FirebaseError",
+      "code": "app/duplicate app"
+    });
+
+    new FirebaseAdapter({foo: "bar"});
+
+    sinon.assert.notCalled(addNotification);
   });
 });
