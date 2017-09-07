0227f161a970aff1f970150e87471d292d1cf850
eddi89 <eduard.schaf@web.de>
2017-09-07 18:12:52 +0200

    Add missing css file.

--- viewWE/content_scripts/js/lib.js
+++ viewWE/content_scripts/js/lib.js
@@ -1,5 +1,6 @@
 const $ = require('jquery');
 require('jquery-ui/ui/widgets/dialog');
+require('jquery-ui/themes/base/button.css');
 require('jquery-ui/themes/base/core.css');
 require('jquery-ui/themes/base/theme.css');
 require('jquery-ui/themes/base/dialog.css');
