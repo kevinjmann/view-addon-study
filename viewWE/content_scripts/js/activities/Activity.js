3e99ea53de26fe84e508dfafb8aaed97e0629035
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-09-19 21:43:32 +0200

    Add activity class

---
+++ viewWE/content_scripts/js/activities/Activity.js
@@ -0,0 +1,40 @@
+import view from '../view';
+import Enhancer from './Enhancer';
+import Selections from './Selections';
+
+const addSelectionPickerToToolbar = (selectionPicker) => {
+  const enhance = document.querySelector('#wertiview-toolbar-enhance-button');
+  const toolbar = document.querySelector('#wertiview-toolbar');
+  toolbar.insertBefore(selectionPicker, enhance);
+};
+
+export default class Activity {
+  constructor(spec, language) {
+    this.spec = spec[language];
+
+    this.selections = new Selections(this.spec.selections);
+    addSelectionPickerToToolbar(this.selections.render());
+
+    this.enhancer = null;
+  }
+
+  selectEnhancement(enhancement) {
+    if (!this.spec[enhancement]) {
+      view.notification.add(`The enhancement ${enhancement} is unavailable for ${this.spec.title}.`);
+      return;
+    }
+
+    this.enhancer = new Enhancer(enhancement, this.spec[enhancement]);
+  }
+
+  runActivity() {
+    if (this.enhancer === null) {
+      view.notification.add('Please select an enhancement type first.');
+      return;
+    }
+
+    const enhancer = this.enhancer;
+    enhancer.enhance(this.selections.getSelections());
+    this.selections.onUpdate((newSelections) => enhancer.update(newSelections));
+  }
+}
