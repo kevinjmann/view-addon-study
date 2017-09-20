417d9ea0f54a6ac9566d7b352a0d58db9e72b493
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-09-20 08:58:39 +0200

    Add more functionality to ActivityPicker

--- viewWE/content_scripts/js/ActivityPicker.js
+++ viewWE/content_scripts/js/ActivityPicker.js
@@ -1,9 +1,40 @@
 import fireEvent from './Events';
 
+const getActivityList = function(activities) {
+  const options = [];
+
+  Object.keys(activities).map((activityKey) => {
+    options.push({
+      value: activityKey,
+      title: activities[activityKey].instruction.title
+    });
+  });
+
+  return options;
+};
+
+const renderOption = ({ title, value }) => {
+
+};
+
+const renderSelect = (activityList) => {
+  const selectElement = document.createElement('select');
+  selectElement.setAttribute('id', 'activityV2-picker');
+  selectElement.classList.add('wertiview-toolbar-menu');
+
+  activityList.map(activity => {
+    const option = renderOption(activity);
+    selectElement.append(option);
+  });
+
+  return selectElement;
+};
+
 export default class ActivityPicker {
   construct(activities) {
     this.listeners = [];
     this.activities = activities;
+    this.oldSelector = document.querySelector('#wertiview-toolbar-activity-menu');
   }
 
   onActivitySelected(f) {
@@ -12,6 +43,16 @@ export default class ActivityPicker {
 
   render() {
     // render options and register fireEvent on this.listeners for onchange
+    const select = renderSelect(getActivityList(this.activities));
+
     // remove old activity selector
+    this.oldSelector.classList.add('hidden');
+  }
+
+  // reinstate old activity selector
+  destroy() {
+    document.querySelector('activityV2-picker').remove();
+    this.listeners = [];
+    this.oldSelector.classList.remove('hidden');
   }
 }
