93a0ebee94ad390c81232b961f735978f0b3357e
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-10-17 18:48:46 +0200

    Add next/last for input enhancement

--- viewWE/content_scripts/js/V2/Activity/Enhancements/Cloze.js
+++ viewWE/content_scripts/js/V2/Activity/Enhancements/Cloze.js
@@ -7,16 +7,36 @@ const createInput = () => {
 };
 
 export default class Cloze {
+  constructor() {
+    this.last = { setAttribute: () => undefined };
+  }
+
   enhance(node) {
     node.innerHTML = '';
     const input = createInput();
     node.append(input);
     const clearNode = () => this.clear(node);
+
+    input.setAttribute('placeholder', node.getAttribute('data-lemma'));
+    this.last.setAttribute('data-view-next', input);
+    input.setAttribute('data-view-previous', this.last);
+    this.last = node;
+
+    const that = this;
     input.onchange = () => {
       const correctAnswer = node.getAttribute('data-original-text');
       if (correctAnswer === input.value) {
         clearNode();
         node.classList.add('view-cloze-correct');
+        const next = input.getAttribute('data-view-next');
+        const previous = input.getAttribute('data-view-previous');
+        if (next) {
+          window.setTimeout(() => next.focus(), 500);
+        }
+        if (next && previous) {
+          next.setAttribute('data-view-previous', previous);
+          previous.setAttribute('data-view-next', next);
+        }
       } else {
         input.classList.add('view-cloze-incorrect');
       }
