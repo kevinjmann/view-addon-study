630f3f43d41ad8f199c02b661586c0bd265e7e2c
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-10-31 23:22:42 +0100

    Push activitySelect out of topicView
    now part of selections

--- viewWE/content_scripts/js/V2/TopicView.js
+++ viewWE/content_scripts/js/V2/TopicView.js
@@ -1,11 +1,10 @@
 export default class TopicView {
-  constructor(activityPicker, selections) {
+  constructor(selections) {
     this.shown = false;
 
     this.toolbar = document.getElementById('wertiview-toolbar');
     this.enhanceButton = document.getElementById('wertiview-toolbar-enhance-button');
     this.activityMenu = document.querySelector('#wertiview-toolbar-activity-menu');
-    this.activitySelect = activityPicker.render();
     this.selectionsWindow = selections.render();
     this.indicator = document.createElement('label');
   }
@@ -15,7 +14,6 @@ export default class TopicView {
     this.activityMenu.classList.add('hidden');
     this.enhanceButton.classList.add('hidden');
 
-    this.toolbar.insertBefore(this.activitySelect, this.enhanceButton);
     this.toolbar.insertBefore(this.indicator, this.enhanceButton);
     this.toolbar.append(this.selectionsWindow);
   }
@@ -25,7 +23,6 @@ export default class TopicView {
       this.activityMenu.classList.remove('hidden');
       this.enhanceButton.classList.remove('hidden');
 
-      this.activitySelect.parentNode.removeChild(this.activitySelect);
       this.selectionsWindow.parentNode.removeChild(this.selectionsWindow);
       this.indicator.parentNode.removeChild(this.indicator);
     }
