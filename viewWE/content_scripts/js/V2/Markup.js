5317d7017158ef104283eac545cd2a9411b341d1
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-11-02 12:44:21 +0100

    Return a status indicator stream for markup

--- viewWE/content_scripts/js/V2/Markup.js
+++ viewWE/content_scripts/js/V2/Markup.js
@@ -1,5 +1,6 @@
 import morph from 'nanomorph';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 function createNode(string, id) {
   const node = document.createElement('div');
@@ -15,6 +16,8 @@ function getContentElement() {
 export default (server, topicConfiguration) => {
   const original = getContentElement().innerHTML;
 
+  const status = new Subject();
+
   topicConfiguration
   // inject special value 'restore' if we're navigating away from V2
     .scan((previous, next) => {
@@ -28,10 +31,12 @@ export default (server, topicConfiguration) => {
   // Emit markup from server fetch request, or original if we're 'restore'ing
     .switchMap((value) => {
       if (value === 'restore') {
+        status.next('restoring');
         return Observable.from([original]);
       }
 
       const { topic: { topic }, language } = value;
+      status.next('fetching markup');
       return Observable.fromPromise(
         server.view({
           topic,
@@ -41,11 +46,23 @@ export default (server, topicConfiguration) => {
           filter: 'no-filter',
           document: original,
         })
-      ).catch(error => console.log('error fetching markup', error));
+      ).catch(error => {
+        status.next('error fetching markup, check console');
+        console.log('error fetching markup', error);
+        return Observable.from([null]);
+      });
     })
-  // Transform HTML document according to markup
-    .subscribe(markup => morph(
-      getContentElement(),
-      createNode(markup, 'wertiview-content')
-    ));
+  // Transform HTML document with to markup
+    .subscribe((markup) => {
+      if (markup) {
+        status.next('replacing markup');
+        morph(
+          getContentElement(),
+          createNode(markup, 'wertiview-content')
+        );
+        status.next('markup done');
+      }
+    });
+
+  return status;
 };
