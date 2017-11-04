ed1341bcb550958a5bb2d04afdb35bcda6906fe7
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-11-04 17:30:43 +0100

    Remove selection rendering from TopicView

--- viewWE/content_scripts/js/V2/TopicView.js
+++ viewWE/content_scripts/js/V2/TopicView.js
@@ -1,11 +1,10 @@
 export default class TopicView {
-  constructor(selections) {
+  constructor() {
     this.shown = false;
 
     this.toolbar = document.getElementById('wertiview-toolbar');
     this.enhanceButton = document.getElementById('wertiview-toolbar-enhance-button');
     this.activityMenu = document.querySelector('#wertiview-toolbar-activity-menu');
-    this.selectionsWindow = selections.render();
     this.indicator = document.createElement('label');
   }
 
@@ -15,15 +14,12 @@ export default class TopicView {
     this.enhanceButton.classList.add('hidden');
 
     this.toolbar.insertBefore(this.indicator, this.enhanceButton);
-    this.toolbar.append(this.selectionsWindow);
   }
 
   hide() {
     if (this.shown) {
       this.activityMenu.classList.remove('hidden');
       this.enhanceButton.classList.remove('hidden');
-
-      this.selectionsWindow.parentNode.removeChild(this.selectionsWindow);
       this.indicator.parentNode.removeChild(this.indicator);
     }
     this.shown = false;
