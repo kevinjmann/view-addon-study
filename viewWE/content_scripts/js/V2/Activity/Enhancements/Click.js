91e0fac33a441b89135953e3c3e3eaa1f7792aee
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-11-05 12:12:52 +0100

    Use a click stream for click instead of classes

--- viewWE/content_scripts/js/V2/Activity/Enhancements/Click.js
+++ viewWE/content_scripts/js/V2/Activity/Enhancements/Click.js
@@ -1,28 +1,41 @@
+import { Observable } from 'rxjs/Observable';
+
+function checkAnswer(enhancement) {
+  return enhancement.getAttribute('data-view-selected') === "true"
+    ? 'view-click-correct'
+    : 'view-click-incorrect';
+}
+
 export default class Click {
-  construct() {
-    console.log('constructing click');
-    this.allEnhancements = document.querySelectorAll('viewEnhancement');
-    for (const enhancement in this.allEnhancements) {
-      enhancement.classList.add('view-click');
-      enhancement.onclick = () => {
-        enhancement.classList.add('view-click-wrong');
-      };
-    }
+  constructor() {
+    this.allEnhancements = [];
+    const addEnhancement = this.addEnhancement.bind(this);
+    this.clickStream = Observable.fromEvent(document, 'click')
+      .filter(event => event.target.tagName === 'VIEWENHANCEMENT')
+      .map(event => event.target)
+      .subscribe(enhancement => addEnhancement(enhancement));
+  }
+
+  addEnhancement(enhancement) {
+    this.allEnhancements.push(enhancement);
+    enhancement.classList.add(checkAnswer(enhancement));
   }
 
   enhance(node) {
-    node.onclick = () => {
-      node.classList.add('view-click-correct');
-    };
+    node.setAttribute('data-view-selected', true);
   }
 
   clear(node) {
+    node.removeAttribute('data-view-selected');
   }
 
   destroy() {
-    for (const enhancement in this.allEnhancements) {
-      enhancement.onclick = undefined;
-      enhancement.classList.remove('view-click-wrong', 'view-click-correct', 'view-click');
-    }
+    this.clickStream.unsubscribe();
+    this.allEnhancements.forEach(
+      enhancement => enhancement.classList.remove(
+        'view-click-incorrect',
+        'view-click-correct'
+      )
+    );
   }
 }
