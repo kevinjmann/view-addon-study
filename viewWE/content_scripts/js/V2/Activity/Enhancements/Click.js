0da758ef596a3549b4d4f5941c8be06179854e95
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2017-11-05 20:41:02 +0100

    Add visual feedback for click activity

--- viewWE/content_scripts/js/V2/Activity/Enhancements/Click.js
+++ viewWE/content_scripts/js/V2/Activity/Enhancements/Click.js
@@ -1,10 +1,17 @@
 import { Observable } from 'rxjs/Observable';
+import Css from './Css';
 
 function checkAnswer(enhancement) {
   return enhancement.getAttribute('data-view-selected') === "true"
     ? 'view-click-correct'
     : 'view-click-incorrect';
 }
+
+const hoverCss = `viewEnhancement:hover {
+  box-shadow: 0px 0px 5px 0px rgba(180, 100, 0, 0.2);
+  background-color: rgba(180, 100, 0, 0.2);
+}`;
+const pointerCss = 'viewEnhancement { cursor: pointer; }';
 
 export default class Click {
   constructor() {
@@ -14,6 +21,9 @@ export default class Click {
       .filter(event => event.target.tagName === 'VIEWENHANCEMENT')
       .map(event => event.target)
       .subscribe(enhancement => addEnhancement(enhancement));
+    this.css = new Css();
+    this.css.apply(hoverCss);
+    this.css.apply(pointerCss);
   }
 
   addEnhancement(enhancement) {
@@ -30,6 +40,7 @@ export default class Click {
   }
 
   destroy() {
+    this.css.remove();
     this.clickStream.unsubscribe();
     this.allEnhancements.forEach(
       enhancement => enhancement.classList.remove(
