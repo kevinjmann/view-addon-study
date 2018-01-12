e3319e1f31690a4b93c0232824829d95c58379c8
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2018-01-12 12:31:26 +0100

    Wire up Markup to new stream format

--- viewWE/content_scripts/js/V2/index.js
+++ viewWE/content_scripts/js/V2/index.js
@@ -7,21 +7,21 @@ import subscribeMarkup from './Markup';
 import subscribeEnhancer from './Activity/Enhancer';
 import statusObservable from './Status';
 import { Subject } from 'rxjs/Subject';
+import control from './Control';
 
 const initialize = async chrome => {
   const browser = new Browser(chrome);
   const { serverURL } = await browser.storage.local.get('serverURL');
   const server = new ViewServer(serverURL);
-  const toolbarConfiguration = makeToolbarConfiguration(view.topics);
+  const commands = control(view.topics);
 
   const toolbar = document.getElementById('wertiview-toolbar');
 
-  const markup = subscribeMarkup(server, toolbarConfiguration);
-  const selections = subscribeTopicInterface(toolbarConfiguration, toolbar);
+  const markup = subscribeMarkup(commands, server);
+  const selections = subscribeTopicInterface(commands, toolbar);
   const enhancer = subscribeEnhancer(selections.concatAll(), markup);
-  const statusDisplay = statusObservable({ markup, enhancer });
 
-  toolbarConfiguration.connect(); // TODO unsubscribe on Toolbar closing
+  const statusDisplay = statusObservable({ markup, enhancer });
 };
 
 export default initialize;
